080b606bfc4cf0052da65cc40b1883f5815f062c
PNZBEIJINGL <yong620yong@sohu.com>
2019-07-04 09:37:42 +0800

    201907040737

--- src/main/scala/com/collection/ScalaListTest.scala
+++ src/main/scala/com/collection/ScalaListTest.scala
@@ -1,5 +1,29 @@
 package com.collection
 
 object ScalaListTest {
+  def main(args: Array[String]): Unit = {
 
+    //列表定义后是不可变的, 且是一个链表结构、
+    //构造列表的两个基本单位是 Nil 和 ::
+
+    val num = (1 :: (2 :: (3 :: Nil)))
+    val empty = Nil;
+    println("num.head=" + num.head);
+    println("num.tail=" + num.tail);
+    println("num.isEmpty=" + num.isEmpty);
+    println("num.size=" + num.size);
+    println("empty.isEmpty=" + empty.isEmpty);
+
+    val emptyList: List[Nothing] = List()
+    val stringList: List[String] = List("It", "is", "a", "sunday");
+    println("print test:" + stringList);
+
+    val fillList = List.fill(3)("7");
+    println("fill test:" + fillList);
+
+    println("List.tabulate() 方法是通过给定的函数来创建列表,起始值为0");
+    val tabulateList = List.tabulate(5)(n => n * n);
+    println(tabulateList);
+
+  }
 }
